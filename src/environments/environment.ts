511f727a37fa7f3d1106dd727f3acb6cff7c9dd3
Karen Makaryan <karenmakaryan@karen-makaryan.local>
2024-01-11 18:52:17 +0400

    env for dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  ApiUrl: 'https://snap-talk-api.vercel.app/',
-  ImagesUrl: 'https://snap-talk-api.vercel.app/aws/images/',
+  ApiUrl: 'http://localhost:3000/',
+  ImagesUrl: 'http://localhost:3000/aws/images/'
 };
